260ff111a459738227612d1b2e1e240eae2288d3
Roger Villanueva <YOYO@Veronicas-Air.grandenetworks.net>
2019-12-04 19:18:47 -0600

    Removed facebook links and modified 6fa description

--- js/main.js
+++ js/main.js
@@ -78,11 +78,9 @@ function introAnimations() {
       $("#imroger").animate({opacity:1}, 1000, null, function() {
         $("#introtext").animate({opacity:1}, 2000, null, function() {
           $("#fadinglineintro").animate({opacity:1}, 500, null, function() {
-            $("#link1").animate({opacity:1, top:0}, 250, null, function() {
-              $("#link2").animate({opacity:1, top:0}, 250, null, function() {
-                $("#link3").animate({opacity:1, top:0}, 250, null, function() {
-                  $("#link4").animate({opacity:1, top:0}, 250);
-                });
+            $("#link2").animate({opacity:1, top:0}, 250, null, function() {
+              $("#link3").animate({opacity:1, top:0}, 250, null, function() {
+                $("#link4").animate({opacity:1, top:0}, 250);
               });
             });
           });
